e4ecc44eed07f7544d258cd7a0e1a9bc32b22f67
Dhiraj Jadhav <dhiraj.jadhav9767@gmail.com>
2025-09-02 17:18:59 +0530

    removed spaces from files

--- src/practiceOops/EvenNumber.java
+++ src/practiceOops/EvenNumber.java
@@ -5,10 +5,14 @@ public class EvenNumber {
     public static void main(String[] args) {
 
         for (int i = 2; i <= 50; i++) {
+
+
             if (i % 2 == 0) {
                 System.out.println(i);
             }
 
+
         }
+
     }
 }
