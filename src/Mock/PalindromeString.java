a147efc35beb8faa6b0cddc8722f9d4535b1072c
Dhiraj Jadhav <dhiraj.jadhav9767@gmail.com>
2025-09-10 16:30:09 +0530

    removed spaces from files

--- src/Mock/PalindromeString.java
+++ src/Mock/PalindromeString.java
@@ -6,27 +6,18 @@ public class PalindromeString {
     public static void main(String[] args) {
 
         System.out.println("enter your name");
-
         Scanner sc = new Scanner(System.in);
-
         String name = sc.nextLine();
-
-
         String Reverse = "";
 
-        for (int i = name.length()-1;i>=0 ; i--) {
+        for (int i = name.length() - 1; i >= 0; i--) {
             char ch = name.charAt(i);
-
-
-            Reverse = Reverse+name.charAt(i);
-
+            Reverse = Reverse + name.charAt(i);
         }
-        if (name.equals(Reverse)){
-            System.out.println(name +" : it is a palindrome ");
-
-        }else{
-
-            System.out.println(name +" : it is a Not-palindrome ");
+        if (name.equals(Reverse)) {
+            System.out.println(name + " : it is a palindrome ");
+        } else {
+            System.out.println(name + " : it is a Not-palindrome ");
 
         }
     }
--- src/Raugh/hellow.java
+++
@@ -1,8 +0,0 @@
-package Raugh;
-
-public class hellow {
-    public static void main(String[] args) {
-
-        System.out.println("Dhiruuu");
-    }
-}
